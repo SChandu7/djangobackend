a65c6b5e426a5c8fa46cf804162f94c665776f2b
S Chandra Sekhar <kingchandus143@gmail.com>
2025-06-26 01:38:50 +0530

    Add files via upload

---
+++ firstdemoapp2/firebase_config.py
@@ -0,0 +1,12 @@
+# yourapp/firebase_config.py
+import firebase_admin
+from firebase_admin import credentials
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+cred = credentials.Certificate(os.path.join(BASE_DIR, 'firstapp2', 'serviceAccountKey.json'))
+
+# Initialize only once
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
