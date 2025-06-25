1011c2fb84056b2c97dcf7654ed925a8004c8764
S Chandra Sekhar <kingchandus143@gmail.com>
2025-06-25 14:34:12 +0530

    adding aws sports activityimages

--- firstdemoapp2/serializers.py
+++ firstdemoapp2/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import todouser,daysandassignments,arduinodata
+from .models import todouser,daysandassignments,arduinodata,SportsDailyActivity,SportsDailyActivityImages
 
 class UserDataSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,3 +14,19 @@ class ArduinoDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = arduinodata
         fields = ['time', 'result']
+
+
+class SportsDailyActivityImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SportsDailyActivityImages
+        fields = ['image_url']
+
+
+class SportsDailyActivitySerializer(serializers.ModelSerializer):
+    images = SportsDailyActivityImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = SportsDailyActivity
+        fields = ['id', 'school', 'date', 'time', 'pt_name', 'activity_type', 'game_name', 'images']
+
+
