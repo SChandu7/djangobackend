1b6c73205dfee2b16a2df217cae878650146f2f0
S Chandra Sekhar <kingchandus143@gmail.com>
2025-06-26 18:15:27 +0530

    Update firebase_config.py

--- firstapp2/firebase_config.py
+++ firstapp2/firebase_config.py
@@ -1,12 +1,13 @@
-# yourapp/firebase_config.py
 import firebase_admin
 from firebase_admin import credentials
 
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if not firebase_admin._apps:
+    cred = credentials.Certificate("/home/ubuntu/djangobackend/sportsforchangeproject-firebase-adminsdk-8u6av-886cee7b42.json")
+    firebase_admin.initialize_app(cred)
 
 
-firebase_admin.initialize_app(credentials.ApplicationDefault())
+
+
 
 
 
