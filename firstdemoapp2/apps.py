0c7bc506ba44321165b6a7aed9effe31b9eb9874
S Chandra Sekhar <kingchandus143@gmail.com>
2025-06-26 23:39:12 +0530

    Update apps.py

--- firstdemoapp2/apps.py
+++ firstdemoapp2/apps.py
@@ -1,13 +1,1 @@
-from django.apps import AppConfig
-import firebase_admin
-from firebase_admin import credentials, initialize_app
 
-if not firebase_admin._apps:
-    cred = credentials.Certificate('/home/ubuntu/djangobackend/firstapp2/sportsforchangeproject-firebase-adminsdk-8u6av-886cee7b42.json')
-    initialize_app(cred)
-
-
-
-class Firstdemoapp2Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'firstdemoapp2'
