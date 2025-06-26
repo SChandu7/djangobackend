573ff34eda56fd5c63ec6d872dbb1debda0a46d1
S Chandra Sekhar <kingchandus143@gmail.com>
2025-06-26 21:01:03 +0530

    Update firebase_config.py

--- firstdemoapp2/firebase_config.py
+++ firstdemoapp2/firebase_config.py
@@ -5,7 +5,7 @@ from firebase_admin import credentials
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-cred = credentials.Certificate(os.path.join(BASE_DIR, 'firstapp2', 'serviceAccountKey.json'))
+cred = credentials.Certificate(os.path.join(BASE_DIR, 'firstapp2', 'sportsforchange-35208-firebase-adminsdk-vs78f-4ba6d80413.json'))
 
 # Initialize only once
 if not firebase_admin._apps:
