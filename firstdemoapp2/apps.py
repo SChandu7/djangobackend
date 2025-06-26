9a1a718d72fedc339e4474b9e58de96f74891443
S Chandra Sekhar <kingchandus143@gmail.com>
2025-06-26 21:27:01 +0530

    Update apps.py

--- firstdemoapp2/apps.py
+++ firstdemoapp2/apps.py
@@ -1,4 +1,11 @@
 from django.apps import AppConfig
+import firebase_admin
+from firebase_admin import credentials, initialize_app
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate('/home/ubuntu/djangobackend/firstapp2/sportsforchangeproject-firebase-adminsdk-8u6av-886cee7b42.json')
+    initialize_app(cred)
+
 
 
 class Firstdemoapp2Config(AppConfig):
