5059b285c7130d78b4b5e3c9c139281e6389b7da
S Chandra Sekhar <kingchandus143@gmail.com>
2025-06-26 23:39:39 +0530

    Update firebase_config.py

--- firstapp2/firebase_config.py
+++ firstapp2/firebase_config.py
@@ -1,9 +1,9 @@
-import firebase_admin
-from firebase_admin import credentials
+# import firebase_admin
+# from firebase_admin import credentials
 
-if not firebase_admin._apps:
-    cred = credentials.Certificate("/home/ubuntu/djangobackend/sportsforchangeproject-firebase-adminsdk-8u6av-886cee7b42.json")
-    firebase_admin.initialize_app(cred)
+# if not firebase_admin._apps:
+#     cred = credentials.Certificate("/home/ubuntu/djangobackend/sportsforchangeproject-firebase-adminsdk-8u6av-886cee7b42.json")
+#     firebase_admin.initialize_app(cred)
 
 
 
