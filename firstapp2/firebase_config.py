9740efdbde8148dfdb3217b04a809b2d3fdba9ec
S Chandra Sekhar <kingchandus143@gmail.com>
2025-06-26 14:13:28 +0530

    Update firebase_config.py

--- firstapp2/firebase_config.py
+++ firstapp2/firebase_config.py
@@ -5,8 +5,8 @@ from firebase_admin import credentials
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-cred = credentials.Certificate(os.path.join(BASE_DIR, 'firstapp2', 'serviceAccountKey.json'))
-firebase_admin.initialize_app(cred)
+
+firebase_admin.initialize_app(credentials.ApplicationDefault())
 
 
 
